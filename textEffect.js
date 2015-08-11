d29037612ddce1696bc29859123b7574c214504a
Andreas Oxinos <oxinosa@gmail.com>
2015-08-11 15:39:56 +0300

    test commit

--- textEffect.js
+++ textEffect.js
@@ -1,22 +1,108 @@
-var message = "Hello World!";
+var messageObj2 = [
+    {text:"", interval:50},
+    {text:"//Lets make some code", interval:50},
+    {text:"document.body.style.backgroundColor = '#C0C0C0'", code:"changeBackgroundColor()", interval:50},
+    {text:"//Lets move this block down, so we see better", interval:50},
+    {text:"moveDown();", code:"movePreDown()", interval:50, Delay:2000},
+    {text:"makeShorter();", code:"makeShorter()", interval:50, Delay: 2000},
+    {text:"makeFatter();", code:"makeFatter()", interval:50, Delay:2000},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+    {text:"//This is text to test editor", interval:20},
+];
 
-function TextEffect() {
-    this.showText = function(target, message, index, interval) {
 
-        if(index < message.length) {
-            document.getElementById("indicator").style.display = "block";
-            document.getElementById(target).innerHTML += message[index++];
-            setTimeout(function(){
-                new TextEffect().showText(target, message, index, interval);
-            }, interval);
-        } else {
-            document.getElementById("indicator").style.display = "none";
-        }
+
+
+function SetText(row, index, initObj){
+    var domElement = document.getElementById(initObj.domTargetId);
+
+        setTimeout(function () {
+
+            if (index < row.text.length) {
+                domElement.scrollIntoView(false);
+                domElement.innerHTML += row.text[index++];
+                SetText(row, index, initObj);
+            } else {
+                eval(initObj.messageArray[initObj.index++].code);
+
+                if(row.Delay) {
+                    setTimeout(function(){
+                        showNext(initObj);
+                    }, row.Delay)
+                }else {
+                    showNext(initObj);
+                }
+
+            }
+
+        }, row.interval);
+}
+
+function showNext(initObj) {
+    if(initObj.index <= initObj.messageArray.length -1) {
+        var _text = initObj.messageArray[initObj.index].text + "\n";
+        var _row = initObj.messageArray[initObj.index];
+        _row.text += "\n";
+        SetText(_row, 0, initObj);
+    } else {
+        console.log("Done");
     }
+}
+
+function start(){
+    var initObject = {
+        messageArray:messageObj2,
+        index:0,
+        domTargetId:"msg"
+    };
+    showNext(initObject);
+}
+
+function changeBackgroundColor(){
+    document.body.style.backgroundColor = "#C0C0C0 ";
+}
+
+function createDiv() {
+    var _div = document.createElement('div');
+    _div.classList.add("newDivPosition");
+    document.body.appendChild(_div);
+
+}
+
+function movePreDown() {
+    var _b = document.getElementById("terminal");
+    _b.classList.add("moveToBottom");
+
+}
+
+function makeShorter() {
+    var _b = document.getElementById("terminal");
+    _b.classList.add("height200px");
+}
+
+function makeFatter() {
+    var _b = document.getElementById("terminal");
+    _b.classList.add("width600px");
 }
 
 
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    new TextEffect().showText("msg", message, 0, 100);
-});
+(function(){
+    start();
+    //createDiv();
+})();
